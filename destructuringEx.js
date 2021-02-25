31c3535bf88c27ef370df0f696fd3714ded3d3e3
Nabin <nkbhaie2017@gmail.com>
2021-02-25 14:49:46 -0700

    add destructuring exercise first solution

---
+++ destructuringEx.js
@@ -0,0 +1,14 @@
+/*    Destructuring Exercise  */
+//Object Destructuring 1
+//What does the following code return/print?
+
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); // ?
+console.log(yearNeptuneDiscovered); // ?
+
+//Answer:
+/*
+console.log(numPlanets) returns 8, and console.log(yearNeptuneDiscovered) returns 1846
+*/
